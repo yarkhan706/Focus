8f5464e85cfab7e6a4ccb40152b73641bdcb0a23
yarkhan706 <yarkhan706@gmail.com>
2023-10-01 14:47:41 +0500

    Started building task history

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import Focus from "./src/features/Focus/focus";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Timer from "./src/features/Timer/timer";
 
 const App = () => {
   const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  useEffect(() => {
+    if(focusSubject){
+      setFocusHistory([...focusHistory, focusSubject]);
+    }
+  }, [focusSubject]);
+
+  console.log(...focusHistory);
+
   return (
     <View style={styles.container}>
       {focusSubject ? (
